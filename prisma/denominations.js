f23dfb21e4e640b6afb1955d2dc1e558dce4fe4d
Zak Fixler <zakfixler@gmail.com>
2023-09-26 19:28:37 -0400

    Init prisma schema, seed db with denominations

---
+++ prisma/denominations.js
@@ -0,0 +1,67 @@
+export const denominations = [
+    { 
+        name: 'The Associate Reformed Presbyterian Church',
+        slug: 'arpc',
+        abbr: 'ARPC',
+    },
+    { 
+        name: 'The Canadian Reformed Churches',
+        slug: 'canrc',
+        abbr: 'CanRC',
+    },
+    { 
+        name: 'The Reformed Church of Quebec',
+        slug: 'erq',
+        abbr: 'erq',
+    },
+    { 
+        name: 'The Free Reformed Churches of North America',
+        slug: 'frcna',
+        abbr: 'FRCNA',
+    },
+    { 
+        name: 'The Heritage Reformed Congregations',
+        slug: 'hrc',
+        abbr: 'HRC',
+    },
+    { 
+        name: 'The Korean American Presbyterian Church',
+        slug: 'kapc',
+        abbr: 'KAPC',
+    },
+    { 
+        name: 'The Korean Presbyterian Church in America (Kosin)',
+        slug: 'kpca',
+        abbr: 'KPCA',
+    },
+    { 
+        name: 'The Orthodox Presbyterian Church',
+        slug: 'opc',
+        abbr: 'OPC',
+    },
+    { 
+        name: 'The Presbyterian Church in America',
+        slug: 'pca',
+        abbr: 'PCA',
+    },
+    { 
+        name: 'The Presbyterian Reformed Church',
+        slug: 'prc',
+        abbr: 'PresRC',
+    },
+    { 
+        name: 'The Reformed Church in the United States',
+        slug: 'rcus',
+        abbr: 'RCUS',
+    },
+    { 
+        name: 'The Reformed Presbyterian Church in North America',
+        slug: 'rpcna',
+        abbr: 'RPCNA',
+    },
+    { 
+        name: 'The United Reformed Churches in North America',
+        slug: 'urcna',
+        abbr: 'URCNA',
+    }
+];
