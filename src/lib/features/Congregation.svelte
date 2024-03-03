e7f90de1a2e4df25af57810f14b39c5ebe9cf02c
Zak Fixler <zakfixler@gmail.com>
2024-03-02 20:56:41 -0500

    Update address labels

--- src/lib/features/Congregation.svelte
+++ src/lib/features/Congregation.svelte
@@ -1,8 +1,7 @@
 <script>
     export let congregation;
-    const { address, contact, email, name, lat, long, pastor, phone, website } = congregation;
-    const uriAddress = encodeURI(address.replace('<br>', ' '));
-    const url = `https://www.google.com/maps/search/?api=1&query=${uriAddress}`;
+    const { address, addressLabel, contact, email, name, lat, long, pastor, phone, website } = congregation;
+    const url = `https://www.google.com/maps/search/?api=1&query=${encodeURI(address)}`;
 </script>
 
 <article class="container">
@@ -15,7 +14,7 @@
             <address class="address">
                 <a href={url} target='_blank' class="link"> 
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon map" height="24" viewBox="0 -960 960 960" width="24"><path d="m600-120-240-84-186 72q-20 8-37-4.5T120-170v-560q0-13 7.5-23t20.5-15l212-72 240 84 186-72q20-8 37 4.5t17 33.5v560q0 13-7.5 23T812-192l-212 72Zm-40-98v-468l-160-56v468l160 56Zm80 0 120-40v-474l-120 46v468Zm-440-10 120-46v-468l-120 40v474Zm440-458v468-468Zm-320-56v468-468Z"/></svg>
-                    {@html address}
+                    {@html addressLabel}
                 </a>
             </address>
             {/if}
