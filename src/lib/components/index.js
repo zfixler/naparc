d2e8f4e036f5df0d309fbfaa1436e5c66d163944
Zak Fixler <zakfixler@gmail.com>
2024-04-24 20:47:17 -0400

    Refactor Phone as component

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,3 +1,4 @@
 export { default as Address } from './Address.svelte';
 export { default as Website } from './Website.svelte';
 export { default as Contact } from './Contact.svelte';
+export { default as Phone } from './Phone.svelte';
