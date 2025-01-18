f3a3d46302875889de525a37efc60f5bffc8a315
Zak Fixler <zakfixler@gmail.com>
2025-01-18 18:41:45 -0500

    Add scraper for PRC

--- src/server/scrapers/index.js
+++ src/server/scrapers/index.js
@@ -4,6 +4,7 @@ export { default as buildFrcnaDenomination } from './frcna.js';
 export { default as buildHrcDenomination } from './hrc.js';
 export { default as buildOpcDenomination } from './opc.js';
 export { default as buildPcaDenomination } from './pca.js';
+export { default as buildPrcDenomination } from './prc.js';
 export { default as buildRcusDenomination } from './rcus.js';
 export { default as buildRpcnaDenomination } from './rpcna.js';
 export { default as buildUrcnaDenomination } from './urcna.js';
