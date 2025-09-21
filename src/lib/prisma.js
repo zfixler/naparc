7c0380adc52ed8aaed5ad39b085c15856bd4c48b
Zak Fixler <zakfixler@gmail.com>
2025-09-21 08:30:34 -0400

    fix: cache prisma client

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,3 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+// Add a type to globalThis to include 'prisma'
+/**
+ * @type {typeof globalThis & { prisma?: import('@prisma/client').PrismaClient }}
+ */
+const globalForPrismaTyped = globalThis;
+
+export const prisma = globalForPrismaTyped.prisma ?? new PrismaClient();
+
+globalForPrismaTyped.prisma = prisma;
